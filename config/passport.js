1d1d99b34732cf2df8b7f92f934f9d6ca2afef93
IamUsike <sathwikthanmay4@gmail.com>
2026-03-08 00:04:30 +0530

    changed oidc to oauth20 cos profile image

--- config/passport.js
+++ config/passport.js
@@ -1,24 +1,21 @@
 import passport from "passport";
-import GoogleOidcStrategy from "passport-google-oidc";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/user.model.js";
 
 passport.use(
   "google",
-  new GoogleOidcStrategy(
+  new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.REDIRECT_URI,
-      scope: ["openid", "profile", "email"],
     },
-    async function verify(issuer, profile, done) {
+    async function verify(accessToken, refreshToken, profile, done) {
       try {
-        console.log(profile);
         const googleId = profile.id;
         const email = profile.emails?.[0]?.value;
 
         let user = await User.findOne({ googleId });
-
         if (!user) {
           user = await User.create({
             googleId,
@@ -27,7 +24,6 @@ passport.use(
             avatar: profile.photos?.[0]?.value,
           });
         }
-
         return done(null, user);
       } catch (err) {
         done(err, null);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,10 @@ import passport from "passport";
 
 const router = express.Router();
 
-router.get("/google", passport.authenticate("google"));
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] }),
+);
 
 router.get(
   "/google/callback",
